6cec912a57f99761ff6ecba62c0e402d3aa762d2
Spoomer <renerahnfeld@gmail.com>
2022-09-23 20:24:50 +0200

    write running services to json file

--- ExtensibleWorkerService.Dispatcher/Program.cs
+++ ExtensibleWorkerService.Dispatcher/Program.cs
@@ -1,7 +1,17 @@
+using System.Text.Json;
+using ExtensibleWorkerService.Core;
 using ExtensibleWorkerService.Dispatcher;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context,services) => { services.AddWorkerServices(context.Configuration); })
     .Build();
-
+var services = host.Services.GetServices<IHostedService>();
+SerializeWorkers(services);
 await host.RunAsync();
+
+void SerializeWorkers(IEnumerable<IHostedService> hostedServices)
+{
+    var workerTasks = hostedServices.Cast<IWorkerTask>();
+    var json = JsonSerializer.Serialize(workerTasks);
+    File.WriteAllText("runningServices.json", json);
+}
